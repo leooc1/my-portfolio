d3229bd6097ea6c516335bf0d0809d2bb652195a
leooc1 <leonardoocvrb@gmail.com>
2023-08-02 16:10:04 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+import Menu from './Menu'
+
+export default function Header() {
+  return (
+    <>
+      <div className='fixed z-40 flex bg-white h-16 w-full items-center px-10'>
+        <h1 className='text-3xl font-semibold'>&lt;LEONARDO/&gt;</h1>
+        <nav className='w-full md:flex hidden justify-end gap-8 '>
+          <Link className='text-xl hover:font-semibold transition-all w-20 text-center'
+            href='#'>Home</Link>
+          <Link className='text-xl hover:font-semibold transition-all w-20 text-center' href='#projetos'>Projetos</Link>
+          <Link className='text-xl hover:font-semibold transition-all w-20 text-center' href='#sobre'>Sobre</Link>
+          <Link className='text-xl hover:font-semibold transition-all w-20 text-center' href='#contato'>Contato</Link>
+        </nav>
+      </div>
+      <Menu />
+    </>
+  )
+}
---
+++ src/components/Menu.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
+
+export default function Menu() {
+    const [hamburger, setHamburger] = useState(true)
+
+    function toggleMenu() {
+        const icon = document.querySelector('#hmb')
+        icon?.classList.toggle('-rotate-90')
+
+        if (icon?.classList.contains('-rotate-90')) {
+            setHamburger(false)
+        }
+        else { setHamburger(true) }
+
+        const menu = document.querySelector('#menu')
+        menu?.classList.toggle('h-0')
+        menu?.classList.toggle('bg-white')
+        menu?.classList.toggle('h-96')
+        const navbar = document.querySelector('#navbar')
+        setTimeout(() => {
+            navbar?.classList.toggle('hidden')
+            navbar?.classList.toggle('flex')
+        }, 150)
+    }
+
+    return (
+        <>
+            <div id='menu' className={`fixed w-full py-3 transition-menu h-0 top-0 z-40 md:hidden flex justify-center items-center`}>
+                <Image id='hmb' className='top-4 right-5 absolute transition-menu'
+                    src={`${hamburger ? '/icon-menu.svg' : '/icon-close.svg'}`} alt="menu" width={40} height={40} onClick={() => toggleMenu()} />
+                <nav id='navbar' className='text-black hidden flex-col gap-8 text-3xl'>
+                    <Link onClick={() => { toggleMenu() }} href="#" className={` uppercase font-light text-center`}>
+                        Home
+                    </Link>
+                    <Link onClick={() => { toggleMenu() }} href="#projetos" className={` uppercase font-light text-center`}>
+                        Projetos
+                    </Link>
+                    <Link onClick={() => { toggleMenu() }} href="#sobre" className={` uppercase font-light text-center`}>
+                        Sobre
+                    </Link>
+                    <Link onClick={() => { toggleMenu() }} href="#contato" className={` uppercase font-light text-center`}>
+                        Contato
+                    </Link>
+                </nav>
+            </div>
+            <div onClick={() => { toggleMenu() }} className={`w-full h-full ${hamburger ? 'hidden' : 'bg-[#000000a2]'} transition-menu fixed bottom-0 z-30`}></div>
+        </>
+    )
+}
